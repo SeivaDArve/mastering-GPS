a887c82a1546d3f8060b5efa6384dffca371a366
Seiva DArve <flowreshe.seiva.d.arve@gmail.com>
2023-01-24 21:27:52 +0000

    Add: First files that describe what the app is all about

---
+++ mGPS.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Title: mastering-GPS
+# Description: App to list and manipulate GPS places
+
+# Example of opening GPS coordinates with google maps on the web browser
+   # Coordinates example:  10.972272, 76.740454
+   
+   # Using program 'op' to open it
+      op "https://www.google.pt/maps/place/10.972272+76.740454"
+
+   # Using program 'xdg-open' to open it
+      xdg-open "https://www.google.pt/maps/place/10.972272+76.740454"
+
+# uDev: Create a command to calculate distances between to GPS coordinates
